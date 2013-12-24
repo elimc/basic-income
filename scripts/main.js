964c938942b9f927854487589f5f71b8d9ccd2d6
Eli McMakin <elimc184@hotmail.com>
2013-12-23 18:27:35 -0600

    Refactored main.js to simplify the code and add syntactic sugar.

--- scripts/main.js
+++ scripts/main.js
@@ -8,59 +8,63 @@ $(document).ready(function() {
     $("#calculate").click(function () {
 
         // Grab values from revenue section and sum them.
-        var incomeTaxNewRevenue = cleanUp( $("#income-tax-new-revenue").val()),
-            corporateTaxNewRevenue = cleanUp( $("#corporate-tax-new-revenue").val()),
-            ssTaxNewRevenue = cleanUp( $("#ss-tax-new-revenue").val()),
-            medicareTaxNewRevenue = cleanUp( $("#medicare-tax-new-revenue").val()),
-            unimploymentTaxNewRevenue = cleanUp( $("#unimployment-tax-new-revenue").val()),
-            otherRetirementTaxNewRevenue = cleanUp( $("#other-retirement-tax-new-revenue").val()),
-            exciseTaxNewRevenue = cleanUp( $("#excise-tax-new-revenue").val()),
-            estateTaxNewRevenue = cleanUp( $("#estate-tax-new-revenue").val()),
-            customsTaxNewRevenue = cleanUp( $("#customs-tax-new-revenue").val()),
-            depositTaxNewRevenue = cleanUp( $("#deposit-tax-new-revenue").val()),
-            otherTaxNewRevenue = cleanUp( $("#other-tax-new-revenue").val()),
-            revenueTotal = incomeTaxNewRevenue + 
-                    corporateTaxNewRevenue + 
-                    ssTaxNewRevenue + 
-                    medicareTaxNewRevenue +
-                    unimploymentTaxNewRevenue + 
-                    otherRetirementTaxNewRevenue + 
-                    exciseTaxNewRevenue + 
-                    estateTaxNewRevenue +
-                    customsTaxNewRevenue + 
-                    depositTaxNewRevenue + 
-                    otherTaxNewRevenue;
-            $("#revenue-total").val("$" + commaSeparateNumber(revenueTotal) );
-        
+        var revenueValues = [
+            $("#income-tax-new-revenue").val(),
+            $("#corporate-tax-new-revenue").val(),
+            $("#ss-tax-new-revenue").val(),
+            $("#medicare-tax-new-revenue").val(),
+            $("#unimployment-tax-new-revenue").val(),
+            $("#other-retirement-tax-new-revenue").val(),
+            $("#excise-tax-new-revenue").val(),
+            $("#estate-tax-new-revenue").val(),
+            $("#customs-tax-new-revenue").val(),
+            $("#deposit-tax-new-revenue").val(),
+            $("#other-tax-new-revenue").val()
+        ];
+
+        var revenueTotal = 0;
+        $.each( revenueValues, function( i, val ) {
+            revenueTotal += cleanUp( val );
+        } );
+        $("#revenue-total").val("$" + commaSeparateNumber(revenueTotal) );
+
         // Grab values from expense section and sum them.
-        var ssExpenseNew = cleanUp( $("#ss-expense-new").val()),
-            medicareExpenseNew = cleanUp( $("#medicare-expense-new").val()),
-            medicaidExpenseNew = cleanUp( $("#medicaid-expense-new").val()),
-            tarpExpenseNew = cleanUp( $("#tarp-expense-new").val()),
-            otherExpenseNew = cleanUp( $("#other-expense-new").val()),
-            mandatoryTotal = ssExpenseNew + 
-                    medicareExpenseNew + 
-                    medicaidExpenseNew + 
-                    tarpExpenseNew +
-                    otherExpenseNew;
-            $("#mandatory-total").val("$" + commaSeparateNumber(mandatoryTotal) );
-            
-        // Grab the expense values and sum them.
-        var securityExpenseNew = cleanUp( $("#security-expense-new").val()),
-            nonSecurityExpenseNew = cleanUp( $("#non-security-expense-new").val()),
-            discretionaryTotal = securityExpenseNew + 
-                    nonSecurityExpenseNew;
-            $("#discretionary-total").val("$" + commaSeparateNumber(discretionaryTotal) );
-            
-        // Grab value from net interest section and sum the total budget.
-        var netInterest = cleanUp( $("#net-interest-expense").val()),
-            mandatoryTotal = cleanUp( $("#mandatory-total").val()),
-            discretionaryTotal= cleanUp( $("#discretionary-total").val()),
-            totalUBI  = netInterest +
-                    mandatoryTotal + 
-                    discretionaryTotal;
-            $("#ubi-total").val("$" + commaSeparateNumber(totalUBI) );
+        var mandatoryExpenseValues = [
+            $("#ss-expense-new").val(),
+            $("#medicare-expense-new").val(),
+            $("#medicaid-expense-new").val(),
+            $("#tarp-expense-new").val(),
+            $("#other-expense-new").val()
+        ];
+
+        var mandatoryTotal = 0;
+        $.each(mandatoryExpenseValues, function( i, val ) {
+            mandatoryTotal += cleanUp( val );
+        });
+        $("#mandatory-total").val("$" + commaSeparateNumber( mandatoryTotal ) );
         
+        // Grab the expense values and sum them.
+        var discretionaryExpenseValues = [
+            $("#security-expense-new").val(),
+            $("#non-security-expense-new").val()
+        ];
+        
+        discretionaryTotal = 0;
+        $.each( discretionaryExpenseValues, function( i, val ) {
+            discretionaryTotal += cleanUp( val );
+        });
+        $("#discretionary-total").val("$" + commaSeparateNumber(discretionaryTotal) );
+            
+        // Set value of net interest.
+        var netInterest = cleanUp( $("#net-interest-expense").val()),
+        
+        /*
+         * PERFORM THE ACTUAL CALCULATIONS.
+         */
+        
+        // Calculate the UBI value and insert the sum into UBI amount total.
+        totalUBI  = netInterest + mandatoryTotal + discretionaryTotal;
+        $("#ubi-total").val("$" + commaSeparateNumber( totalUBI ) );
         
         var totalExpenseFinal = cleanUp( $("#total-expense-final").val() );
         
@@ -68,18 +72,18 @@ $(document).ready(function() {
         
         var deficitSurplus = revenueTotal - totalExpenseFinal;
         
-        $("#deficit-surplus").val("$" + commaSeparateNumber(deficitSurplus) );
+        $("#deficit-surplus").val("$" + commaSeparateNumber( deficitSurplus ) );
         
         // Grab population section.
         var population = cleanUp( $("#us-citizens").val() );
         
         // Finally, perform the actual calculations to determine the UBI.
         var ubiRaw = totalUBI / population,
-            ubiPerYear = commaSeparateNumber(Math.round(ubiRaw)),
-            ubiPerMonth = commaSeparateNumber(Math.round( ubiRaw / 12 ));
+            ubiPerYear = commaSeparateNumber( Math.round( ubiRaw ) ),
+            ubiPerMonth = commaSeparateNumber( Math.round( ubiRaw / 12 ) );
         
         // Output the calculated result.
-        $("#result").html("$" + ubiPerYear + " per adult per year<br>$" + ubiPerMonth + " per adult per month");
+        $("#result").html ("$" + ubiPerYear + " per adult per year<br>$" + ubiPerMonth + " per adult per month" );
 
     }); // End main calculator.
 
